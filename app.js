8a1607bbae9e1174130106880bd4d24cf40cdb2e
Aamish Hussain <aamishhussain23@gmail.com>
2023-09-18 13:23:34 +0530

    resolved play again button

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (local_storage_user_value == null) {
 }
 
 // stop next button
-function stop_next_button(){
+function stop_next_button() {
   let next_btn = document.querySelector(".next");
   next_btn.style.display = "none";
 }
@@ -32,7 +32,7 @@ function stop_result_show_game() {
 }
 
 // show main and header
-function show_main_and_header(){
+function show_main_and_header() {
   let header = document.querySelector("header");
   header.style.display = "flex";
   let main = document.querySelector("main");
@@ -40,7 +40,7 @@ function show_main_and_header(){
 }
 
 // stop main and header
-function stop_main_and_header(){
+function stop_main_and_header() {
   let main = document.querySelector("main");
   main.style.display = "none";
   let header = document.querySelector("header");
@@ -61,6 +61,7 @@ function hurray_play_again() {
 // show hurray message code
 function show_hurray() {
   stop_main_and_header();
+  stop_next_button();
   let white_box = document.querySelector(".white-box");
   white_box.style.display = "none";
   let game = document.querySelector(".game");
@@ -70,7 +71,7 @@ function show_hurray() {
 }
 
 // stopping all background circle for user
-function stop_all_user_circle(){
+function stop_all_user_circle() {
   let user_circle_1 = document.querySelector(".user-circle-1");
   user_circle_1.style.visibility = "hidden";
   let user_circle_2 = document.querySelector(".user-circle-2");
@@ -80,7 +81,7 @@ function stop_all_user_circle(){
 }
 
 // displaying all background circle for user
-function show_all_user_circle(){
+function show_all_user_circle() {
   let user_circle_1 = document.querySelector(".user-circle-1");
   user_circle_1.style.visibility = "visible";
   let user_circle_2 = document.querySelector(".user-circle-2");
@@ -90,7 +91,7 @@ function show_all_user_circle(){
 }
 
 // stopping all background circle for pc
-function stop_all_pc_circle(){
+function stop_all_pc_circle() {
   let pc_circle_1 = document.querySelector(".pc-circle-1");
   pc_circle_1.style.visibility = "hidden";
   let pc_circle_2 = document.querySelector(".pc-circle-2");
@@ -100,7 +101,7 @@ function stop_all_pc_circle(){
 }
 
 // displaying all background circle for pc
-function show_all_pc_circle(){
+function show_all_pc_circle() {
   let pc_circle_1 = document.querySelector(".pc-circle-1");
   pc_circle_1.style.visibility = "visible";
   let pc_circle_2 = document.querySelector(".pc-circle-2");
@@ -110,7 +111,7 @@ function show_all_pc_circle(){
 }
 
 // stopping game and showing result
-function stop_game_show_result(){
+function stop_game_show_result() {
   let game = document.querySelector(".game");
   game.style.display = "none";
   let result = document.querySelector(".result");
@@ -123,6 +124,8 @@ function show_tieUp(element_name, element_color, msg) {
   stop_all_pc_circle();
   stop_game_show_result();
   stop_next_button();
+  let play_again_btn = document.querySelector('#para-btn')
+  play_again_btn.innerHTML = "REPLAY";
   let user = document.querySelector(".user");
   user.style.border = `15px solid ${element_color}`;
   let computer = document.querySelector(".pc");
@@ -131,14 +134,14 @@ function show_tieUp(element_name, element_color, msg) {
   user_image.src = `resources/${element_name}.png`;
   let computer_image = document.querySelector("#pc-image");
   computer_image.src = `resources/${element_name}.png`;
-  let message1 = document.querySelector('.message-1')
-  message1.textContent = `${msg}`
-  let message2 = document.querySelector('.message-2')
-  message2.textContent = " "
+  let message1 = document.querySelector(".message-1");
+  message1.textContent = `${msg}`;
+  let message2 = document.querySelector(".message-2");
+  message2.textContent = " ";
 }
 
-// setting user button and image after winning 
-function set_user_btn_and_img(user_element_name, user_btn_color){
+// setting user button and image after winning
+function set_user_btn_and_img(user_element_name, user_btn_color) {
   let user = document.querySelector(".user");
   user.style.border = `15px solid ${user_btn_color}`;
   let user_image = document.querySelector("#user-image");
@@ -146,7 +149,7 @@ function set_user_btn_and_img(user_element_name, user_btn_color){
 }
 
 // setting pc button and image after winning
-function set_pc_btn_and_img(pc_element_name, pc_btn_color){
+function set_pc_btn_and_img(pc_element_name, pc_btn_color) {
   let computer = document.querySelector(".pc");
   computer.style.border = `15px solid ${pc_btn_color}`;
   let computer_image = document.querySelector("#pc-image");
@@ -154,27 +157,22 @@ function set_pc_btn_and_img(pc_element_name, pc_btn_color){
 }
 
 // displaying message after winning or loosing
-function display_message(msg, msg2){
-  let message1 = document.querySelector('.message-1')
-  message1.textContent = `${msg}`
-  let message2 = document.querySelector(".message-2")
-  message2.textContent = `${msg2}`
+function display_message(msg, msg2) {
+  let message1 = document.querySelector(".message-1");
+  message1.textContent = `${msg}`;
+  let message2 = document.querySelector(".message-2");
+  message2.textContent = `${msg2}`;
 }
 
 // code for user after winning
-function show_result_for_user(
-  user_btn_color,
-  user_element_name,
-  pc_btn_color,
-  pc_element_name,
-  msg,
-  msg2
-) {
+function show_result_for_user(user_btn_color, user_element_name, pc_btn_color, pc_element_name, msg, msg2) {
   stop_game_show_result();
   show_all_user_circle();
   stop_all_pc_circle();
   set_user_btn_and_img(user_element_name, user_btn_color);
   set_pc_btn_and_img(pc_element_name, pc_btn_color);
+  let play_again_btn = document.querySelector('#para-btn')
+  play_again_btn.innerHTML = "PLAY AGAIN";
   let next_btn = document.querySelector(".next");
   next_btn.style.display = "flex";
   display_message(msg, msg2);
@@ -182,14 +180,7 @@ function show_result_for_user(
 }
 
 // code for pc after winning
-function show_result_for_pc(
-  pc_btn_color,
-  pc_element_name,
-  user_btn_color,
-  user_element_name,
-  msg,
-  msg2
-) {
+function show_result_for_pc(pc_btn_color, pc_element_name, user_btn_color, user_element_name, msg, msg2) {
   stop_game_show_result();
   stop_all_user_circle();
   show_all_pc_circle();
@@ -197,6 +188,8 @@ function show_result_for_pc(
   set_pc_btn_and_img(pc_element_name, pc_btn_color);
   set_user_btn_and_img(user_element_name, user_btn_color);
   display_message(msg, msg2);
+  let play_again_btn = document.querySelector('#para-btn')
+  play_again_btn.innerHTML = "PLAY AGAIN";
   return 1;
 }
 
@@ -221,24 +214,24 @@ function close_popup() {
 }
 
 // getting random element
-function get_random_element(){
+function get_random_element() {
   let element = ["rock", "paper", "scissor"];
   let randomIndex = Math.floor(Math.random() * element.length);
   return element[randomIndex];
 }
 
 // setting pc score
-function set_pc_score(final_value){
+function set_pc_score(final_value) {
   localStorage.setItem("pc-score", final_value);
   let local_storage_value = localStorage.getItem("pc-score");
   pc_score.value = local_storage_value;
 }
 
 // setting user score
-function set_user_score(final_value){
-    localStorage.setItem("user-score", final_value);
-    let local_storage_value = localStorage.getItem("user-score");
-    user_score.value = local_storage_value;
+function set_user_score(final_value) {
+  localStorage.setItem("user-score", final_value);
+  let local_storage_value = localStorage.getItem("user-score");
+  user_score.value = local_storage_value;
 }
 
 // code for ROCK
@@ -249,27 +242,12 @@ function rock() {
     show_tieUp("rock", "#0074B6", "TIE UP");
     return;
   } else if (computerChoice === "paper") {
-    let point = show_result_for_pc(
-      "#ffa943",
-      "paper",
-      "#0074b6",
-      "rock",
-      "YOU LOST",
-      "AGAINST PC"
-    );
+    let point = show_result_for_pc("#ffa943", "paper", "#0074b6", "rock", "YOU LOST", "AGAINST PC");
     let pc_score = document.querySelector(".pc-score");
     let final_value = parseInt(pc_score.value) + point;
     set_pc_score(final_value);
-
   } else {
-    let point = show_result_for_user(
-      "#0074B6",
-      "rock",
-      "#bd00ff",
-      "scissor",
-      "YOU WIN",
-      "AGAINST PC"
-    );
+    let point = show_result_for_user( "#0074B6", "rock", "#bd00ff", "scissor", "YOU WIN", "AGAINST PC");
     let user_score = document.querySelector(".user-score");
     let final_value = parseInt(user_score.value) + point;
     set_user_score(final_value);
@@ -278,31 +256,17 @@ function rock() {
 
 // code for PAPER
 function paper() {
-  let computerChoice = get_random_element()
+  let computerChoice = get_random_element();
   if (computerChoice === "paper") {
     show_tieUp("paper", "#ffa943", "TIE UP");
     return;
   } else if (computerChoice === "rock") {
-    let point = show_result_for_user(
-      "#ffa943",
-      "paper",
-      "#0074b6",
-      "rock",
-      "YOU WIN",
-      "AGAINST PC"
-    );
+    let point = show_result_for_user( "#ffa943", "paper", "#0074b6", "rock", "YOU WIN", "AGAINST PC");
     let user_score = document.querySelector(".user-score");
     let final_value = parseInt(user_score.value) + point;
     set_user_score(final_value);
   } else {
-    let point = show_result_for_pc(
-      "#bd00ff",
-      "scissor",
-      "#ffa943",
-      "paper",
-      "YOU LOST",
-      "AGAINST PC"
-    );
+    let point = show_result_for_pc( "#bd00ff", "scissor", "#ffa943", "paper", "YOU LOST", "AGAINST PC");
     let pc_score = document.querySelector(".pc-score");
     let final_value = parseInt(pc_score.value) + point;
     set_pc_score(final_value);
@@ -316,26 +280,12 @@ function scissor() {
     show_tieUp("scissor", "#bd00ff", "TIE UP");
     return;
   } else if (computerChoice === "rock") {
-    let point = show_result_for_pc(
-      "#0074b6",
-      "rock",
-      "#bd00ff",
-      "scissor",
-      "YOU LOST",
-      "AGAINST PC"
-    );
+    let point = show_result_for_pc("#0074b6","rock","#bd00ff","scissor","YOU LOST","AGAINST PC");
     let pc_score = document.querySelector(".pc-score");
     let final_value = parseInt(pc_score.value) + point;
     set_pc_score(final_value);
   } else {
-    let point = show_result_for_user(
-      "#bd00ff",
-      "scissor",
-      "#ffa943",
-      "paper",
-      "YOU WIN",
-      "AGAINST PC"
-    );
+    let point = show_result_for_user( "#bd00ff", "scissor", "#ffa943", "paper", "YOU WIN", "AGAINST PC");
     let user_score = document.querySelector(".user-score");
     let final_value = parseInt(user_score.value) + point;
     set_user_score(final_value);
